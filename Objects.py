251c7659d6485d980829d02edb41999c6bd82cf1
gplorovg <gplorovg@gmail.com>
2023-10-05 17:03:58 +0300

    complete modifying data structure

--- Objects.py
+++ Objects.py
@@ -1,12 +1,14 @@
+from sys import maxsize
 from dataclasses import dataclass, fields
 
 
 @dataclass
 class Recipe:
     name: str = None
-    ingredients: list[str] = None
+    section: str = None
     price: int = 0
     serving: str = None
+    id: int = 0
 
     def trans_to_iterable(self) -> list:
         resp = list()
@@ -14,17 +16,22 @@ class Recipe:
             resp.append(getattr(self, f.name))
         return resp
 
+    def __post_init__(self):
+        self.id = hash(self.name + self.section) % (maxsize + 1)
+
 
 @dataclass
 class Meal(Recipe):
-    course: str = None
-    alternatives: list[str] = None
-    meal_matches: list[str] = None
-    drink_matches: list[str] = None
+    description: str = None
 
 
 @dataclass
 class Drink(Recipe):
-    value: int = 0
-    alternatives: list[str] = None
-    meal_matches: list[str] = None
+    volume: str = None
+
+
+@dataclass
+class Cocktail(Recipe):
+    ingredients: list[str] = None
+    image_path: str = None
+    volume: str = None
