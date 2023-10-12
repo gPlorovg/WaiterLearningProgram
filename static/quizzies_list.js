59ab95df7c0b7f1fdbd2e6fc451fff8285f5340a
gplorovg <gplorovg@gmail.com>
2023-10-12 20:20:59 +0300

    add turn back button to games list

--- static/quizzies_list.js
+++ static/quizzies_list.js
@@ -1,4 +1,7 @@
-const buttons = document.querySelectorAll("button")
+const buttons = document.querySelectorAll(".game");
+const turn_back_btn = document.querySelector(".turn_back");
+turn_back_btn.addEventListener("click", () => {window.location = window.location.origin + "/main"});
+
 for (let b of buttons) {
-    b.addEventListener("click", () => {window.location = window.location + "/" + b.id})
+    b.addEventListener("click", () => {window.location = window.location + "/" + b.id});
 }
